375bcd5f29095d496854d687bf4cbdefedb6ee77
Samuel S. Watson <samuel.s.watson@gmail.com>
2017-01-31 14:16:08 -0500

    Added reduced row echelon form (rref)

--- src/IntroLinearAlgebra.jl
+++ src/IntroLinearAlgebra.jl
@@ -2,7 +2,7 @@ module IntroLinearAlgebra
 
 # package code goes here
 
-export rowswitch, rowscale, rowadd
+export rowswitch, rowscale, rowadd, rowswitch!, rowscale!, rowadd!, rref
 
 import Base.show
 
@@ -58,5 +58,94 @@ function rowadd{T<:RatOrInt}(M::Array{T,2},i::Integer,j::Integer,x::Real)
     A[i,:] += x*A[j,:]
     return A
 end
+
+function rowswitch!{T<:RatOrInt}(M::Array{T,2},i::Integer,j::Integer)
+    if i == j
+        return nothing
+    else
+        M[i,:], M[j,:] = M[j,:], M[i,:]
+        return nothing
+    end
+end
+
+function rowscale!{T<:RatOrInt}(M::Array{T,2},i::Integer,x::Real)
+    if isa(x,Rational) && issubtype(typeof(M).parameters[1],Integer)
+        error("Initialize your matrix with at least one rational number " *
+       	     "to use rowscale! with a rational, like M = [1//1 2; 3 4]")
+    end
+    M[i,:] *= x
+    return nothing
+end
+
+function rowadd!{T<:RatOrInt}(M::Array{T,2},i::Integer,j::Integer,x::Real)
+    if isa(x,Rational) && issubtype(typeof(M).parameters[1],Integer)
+        error("Initialize your matrix with at least one rational number " *
+       	     "to use rowadd! with a rational, like M = [1//1 2; 3 4]")
+    else
+        M[i,:] += x*M[j,:]
+    end
+    return nothing
+end
+
+
+function rref{T<:RatOrInt}(M::Array{T,2};showsteps=false)
+    A = convert(Array{Rational{Int64},2},M)
+    steps = typeof(A)[]
+    current_row = 1
+    for j=1:size(A,2)
+        if all(A[:,j] .== 0)
+            continue
+        else
+            i = current_row-1 + findfirst(A[current_row:end,j] .!= 0)
+            rowswitch!(A,current_row,i)
+            if showsteps push!(steps,copy(A)) end
+            zerooutbelow!(A,current_row,j)
+            if showsteps push!(steps,copy(A)) end
+            normalizerow!(A,current_row)
+            if showsteps push!(steps,copy(A)) end
+            current_row += 1
+            if current_row > size(A,1)
+                break
+            end
+        end
+    end
+    for i=size(A,1):-1:1
+        if all(A[i,:] .== 0)
+            continue
+        else
+            j = findfirst(A[i,:] .!= 0) 
+            zerooutabove!(A,i,j)
+            if showsteps push!(steps,copy(A)) end
+        end
+    end
+    if showsteps
+        return steps
+    else
+        return A
+    end
+end
+
+function zerooutbelow!{T<:RatOrInt}(M::Array{T,2},i::Integer,j::Integer) # zero out entries below the (i,j)th
+    for k=i+1:size(M,1)
+        rowadd!(M,k,j,-M[k,j]//M[i,j])
+    end
+    return nothing
+end
+
+
+function zerooutabove!{T<:RatOrInt}(M::Array{T,2},i::Integer,j::Integer) 
+    for k=1:i-1
+        rowadd!(M,k,j,-M[k,j]//M[i,j])
+    end
+    return nothing
+end
+
+
+function normalizerow!{T<:RatOrInt}(M::Array{T,2},i::Integer)
+    j = findfirst(M[i,:] .!= 0)
+    rowscale!(M,i,1//M[i,j])
+    return nothing
+end
+
 
 end # module
